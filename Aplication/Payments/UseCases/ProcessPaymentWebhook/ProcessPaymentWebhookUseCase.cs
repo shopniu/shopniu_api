4c3e8538bc2efacfa3ded4af69c8216f79419500
DanielAmado11 <danielamado1107@gmail.com>
2026-08-19 12:58:47 -0500

    feat(webhook): sincronizar estado del delivery con el resultado del pago

--- Aplication/Payments/UseCases/ProcessPaymentWebhook/ProcessPaymentWebhookUseCase.cs
+++ Aplication/Payments/UseCases/ProcessPaymentWebhook/ProcessPaymentWebhookUseCase.cs
@@ -8,11 +8,13 @@ namespace Shopniu_api.Aplication.Payments.UseCases.ProcessPaymentWebhook;
 public class ProcessPaymentWebhookUseCase
 {
     private readonly ITransactionRepository _transactionRepository;
+    private readonly IDeliveryRepository _deliveryRepository;
     private readonly IUnitOfWork _unitOfWork;
 
-    public ProcessPaymentWebhookUseCase(ITransactionRepository transactionRepository, IUnitOfWork unitOfWork)
+    public ProcessPaymentWebhookUseCase(ITransactionRepository transactionRepository, IDeliveryRepository deliveryRepository, IUnitOfWork unitOfWork)
     {
         _transactionRepository = transactionRepository;
+        _deliveryRepository = deliveryRepository;
         _unitOfWork = unitOfWork;
     }
 
@@ -25,6 +27,12 @@ public class ProcessPaymentWebhookUseCase
         }
 
         transaction.UpdatePaymentResult(payload.ProviderTransactionId, payload.Status);
+
+        // El delivery se actualiza según el resultado del pago (transacciones
+        // previas sin delivery se omiten).
+        var delivery = await _deliveryRepository.GetByTransactionIdAsync(transaction.Id);
+        delivery?.UpdateStatusFromTransaction(payload.Status);
+
         await _unitOfWork.SaveChangesAsync();
     }
 }
