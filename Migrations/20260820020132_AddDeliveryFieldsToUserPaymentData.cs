d8a5f86e2ddbc244ccb6a7ea31ac080d64dad8a7
DanielAmado11 <danielamado1107@gmail.com>
2026-08-19 21:03:00 -0500

    chore(db): migración AddDeliveryFieldsToUserPaymentData

---
+++ Migrations/20260820020132_AddDeliveryFieldsToUserPaymentData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopniu_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeliveryFieldsToUserPaymentData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "UserPaymentData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CityCode",
+                table: "UserPaymentData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "UserPaymentData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentCode",
+                table: "UserPaymentData",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "UserPaymentData");
+
+            migrationBuilder.DropColumn(
+                name: "CityCode",
+                table: "UserPaymentData");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "UserPaymentData");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentCode",
+                table: "UserPaymentData");
+        }
+    }
+}
