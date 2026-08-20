fd06335df092b6655322c5e526ddcd40eafa1a42
DanielAmado11 <danielamado1107@gmail.com>
2026-08-19 20:07:01 -0500

    chore(db): migración RenameStateToDepartmentInDeliveries

---
+++ Migrations/20260820010625_RenameStateToDepartmentInDeliveries.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopniu_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameStateToDepartmentInDeliveries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "Deliveries",
+                newName: "Department");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Department",
+                table: "Deliveries",
+                newName: "State");
+        }
+    }
+}
