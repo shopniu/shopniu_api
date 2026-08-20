84a5b32cc92132c88aedbf08319eb9978ad8167b
DanielAmado11 <danielamado1107@gmail.com>
2026-08-20 17:47:11 -0500

    chore(db): migración AddExpiryToUserPaymentData

---
+++ Migrations/20260820224424_AddExpiryToUserPaymentData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopniu_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpiryToUserPaymentData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExpMonth",
+                table: "UserPaymentData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpYear",
+                table: "UserPaymentData",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpMonth",
+                table: "UserPaymentData");
+
+            migrationBuilder.DropColumn(
+                name: "ExpYear",
+                table: "UserPaymentData");
+        }
+    }
+}
