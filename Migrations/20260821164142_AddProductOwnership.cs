e088e67f4b898a250cdbdb6a3736c94b5d862dd1
DanielAmado11 <danielamado1107@gmail.com>
2026-08-21 11:42:49 -0500

    chore(db): migración AddProductOwnership

---
+++ Migrations/20260821164142_AddProductOwnership.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shopniu_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductOwnership : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Products",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ProductOwners",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductOwners", x => new { x.ProductId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_ProductOwners_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductOwners_UserId",
+                table: "ProductOwners",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductOwners");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Products");
+        }
+    }
+}
